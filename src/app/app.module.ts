602253b1d8509159a04e6c02086d7705598bee54
jonathan-ca19 <jap.canar@.edu.ec>
2023-05-30 20:42:36 -0500

    primero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from "./shared/shared.module";
 import { PagesModule } from './pages/pages.module';
+import { NoPagesFoundComponent } from './noPagesFound/no-pages-found/no-pages-found.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        NoPagesFoundComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
